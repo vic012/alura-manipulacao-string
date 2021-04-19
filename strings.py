8fcc962423fa477a2284aa3e0a38b5dbbb561ce4
Pedro Henrique <henriquevic012@gmail.com>
2021-04-19 18:10:53 -0300

    Adicionando métodos, FIND e SPLIT

--- strings.py
+++ strings.py
@@ -4,4 +4,11 @@ print(sobreMim[-2:])'''
 
 url = "https://www.butebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=1500"
 argumento = "moedaorigem=real"
-print(argumento[5:11])
+
+#Método find
+index = argumento.find("=")
+substring = argumento[index + 1:]
+print(substring)
+#Método Split, separa texto por separadores informados
+lista = argumento.split("=")
+print(lista)
