860810faff618907a798f9698389cdc2fac82ab6
Pedro Henrique <henriquevic012@gmail.com>
2021-04-19 19:12:58 -0300

    Atualizando o script

--- strings.py
+++ strings.py
@@ -1,19 +1,16 @@
 #Fatiamento e índices de strings
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 
-url = "https://bytebank.com/cambio?moedaOrigem=real"
-print(url)
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
 
-#Procura pelo caractere ?
-indice = url.find("?")
-
-url_base = url[:indice]
-#print(url_base)
-
-url_parametros = url[indice + 1:]
-#print(url_parametros)
-
-busca = 'moedaOrigem'
-indice_parametro = url_parametros.find(busca)
-indice_valor = indice_parametro + len(busca) + 1
-valor = url_parametros[indice_valor:]
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 print(valor)
