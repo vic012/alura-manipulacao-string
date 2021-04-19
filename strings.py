49a51d36a24303456741c583dc8265ffcc1da251
Pedro Henrique <henriquevic012@gmail.com>
2021-04-19 06:55:48 -0300

    Fatiamento e índices de strings

---
+++ strings.py
@@ -0,0 +1,7 @@
+#Fatiamento e índices de strings
+'''sobreMim = "Meu nome é Pedro e minha idade é 26"
+print(sobreMim[-2:])'''
+
+url = "https://www.butebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=1500"
+argumento = "moedaorigem=real"
+print(argumento[5:11])
