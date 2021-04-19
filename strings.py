a91f1e5e8ae8936e0054e9e9a2650e9f71f998fd
Pedro Henrique <henriquevic012@gmail.com>
2021-04-19 18:54:11 -0300

    Adicionando o método len()

--- strings.py
+++ strings.py
@@ -1,10 +1,19 @@
 #Fatiamento e índices de strings
 
-url = "bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
-print(url_base)
+#Procura pelo caractere ?
+indice = url.find("?")
 
-url_parametros = url[20:]
-print(url_parametros)
+url_base = url[:indice]
+#print(url_base)
+
+url_parametros = url[indice + 1:]
+#print(url_parametros)
+
+busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(busca)
+indice_valor = indice_parametro + len(busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
