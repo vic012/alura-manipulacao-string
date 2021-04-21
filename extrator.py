bfd8faac047592b522343ecd57f3be2b74dfb299
Pedro Henrique <henriquevic012@gmail.com>
2021-04-21 19:04:21 -0300

    Verificando urls vazia

--- extrator.py
+++ extrator.py
@@ -4,10 +4,13 @@ class ExtratorUrl:
 		self.valida_url()
 
 	def sanitiza_url(self, url):
-		return url.strip()
+		if (type(url)==str):
+			return url.strip()
+		else:
+			return ""
 
 	def valida_url(self):
-		if (self.url == ""):
+		if (not self.url):
 			raise ValueError("A URL está vazia")
 
 	def get_url_base(self):
@@ -32,6 +35,7 @@ class ExtratorUrl:
 
 
 #extrator_url = ExtratorUrl(" ")
+#extrator_url = ExtratorUrl(None)
 extrator_url = ExtratorUrl("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
 valor_quantidade = extrator_url.get_valor_parametro('quantidade')
 print(valor_quantidade)
